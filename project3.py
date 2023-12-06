5be850d6ca187580dabc94d46185da3c4ee7fc13
juntan20216 <152441380+juntan20216@users.noreply.github.com>
2023-12-06 16:05:14 -0500

    Update project3.py

--- project3.py
+++ project3.py
@@ -18,7 +18,33 @@ from p3tests import *
 detectArbitrage
 """
 def detectArbitrage(adjList, adjMat, tol=1e-15):
-    ##### Your implementation goes here. #####
+   # Initialize distances and predecessors
+    for vertex in adjList:
+        vertex.dist = float('inf')
+        vertex.prev = None
+    
+    # Relax edges |V| - 1 times
+    for i in range(len(adjList) - 1):
+        for vertex in adjList:
+            for neighbor in vertex.neigh:
+                if vertex.dist + adjMat[vertex.rank][neighbor.rank] < neighbor.dist:
+                    neighbor.dist = vertex.dist + adjMat[vertex.rank][neighbor.rank]
+                    neighbor.prev = vertex
+
+    # Check for negative cycles
+    for vertex in adjList:
+        for neighbor in vertex.neigh:
+            if vertex.dist + adjMat[vertex.rank][neighbor.rank] < neighbor.dist - tol:
+                # Negative cycle found, trace back to get the cycle
+                cycle = []
+                current = neighbor
+                while current not in cycle:
+                    cycle.append(current)
+                    current = current.prev
+                cycle.reverse()  # To get the cycle in the correct order
+                return cycle
+
+    # No arbitrage opportunity found
     return []
     ##### Your implementation goes here. #####
 
